00dda0777b5a40ea59f7a3c0b891ff6e1a712339
unknown <L21412@agit.local>
2022-05-24 09:57:08 +0800

    change file

--- test5.java
+++ test5.java
@@ -8,17 +8,20 @@ public class test5 extends JFrame {
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(500,500);
-        this.setLayout(new BorderLayout());
+        this.setLayout(new BorderLayout(10,10));
 
         JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
 
 
         panel1.setBackground(Color.red);
+        panel2.setBackground(Color.blue);
 
         panel1.setPreferredSize(new Dimension(100,100));
+        panel2.setPreferredSize(new Dimension(100,100));
 
-
-        this.add(panel1,BorderLayout.SOUTH);
+        this.add(panel1,BorderLayout.WEST);
+        this.add(panel2,BorderLayout.NORTH);
 
         //
     }
